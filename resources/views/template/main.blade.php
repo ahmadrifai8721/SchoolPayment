8d2defd7eed5fa6e1a986b7724ceef578d221f77
ahmadrifai <ahmatrifai8721@gmail.com>
2025-01-11 02:45:52 +0700

    update meata

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8" />
     <title>{{ $title }} | {{ env('APP_NAME') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ url('/') }}/assets/images/favicon.ico">
